4fe95499d94230e11ec95b079045bc990f5b7bf9
souleyFall <129006034+souleyFall@users.noreply.github.com>
2025-01-29 09:21:44 +0100

    Test beyable
    Code source
    Index.html
    main.css
    main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+/**
+ * cache le pop-in au démarrage de la plage
+ */
+document.getElementById('overlay').style.display='none';
+
+/**
+ * affiche le pop-in après 4 secondes de délai
+ */
+setTimeout(() => {
+    document.getElementById('overlay').style.display='flex';
+}, 3000);
+
+/* fait disparaitre le pop-in */
+function close_overlay(){
+    document.getElementById('overlay').style.display='none';
+}
+
+/* fait apparaitre le pop-in */
+function open_overlay(){
+    document.getElementById('overlay').style.display='flex';
+}
